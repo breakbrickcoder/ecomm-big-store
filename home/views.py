93246ac8a09ee60ac7fbf6317583b4588ff0475b
breakbrickcoder <davidgurung826@gmail.com>
2021-10-02 15:20:08 +0545

    subcategory

--- home/views.py
+++ home/views.py
@@ -12,7 +12,12 @@ class HomeView(BaseView):
 		self.views['subcategories'] = Subcategory.objects.all()
 		self.views['sliders'] = Slider.objects.all()
 
-
 		return render(request,'index.html',self.views)
+
+class SubCategory(BaseView):
+	def get(self,request,slug):
+		ids = Subcategory.objects.get(slug = slug).id
+		self.views['subcat_items'] = Item.objects.filter(subcategory_id = ids)
+		return render(request,'subcategory.html',self.views)
 		
 
